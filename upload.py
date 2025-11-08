939736ea01fad54a53d6e64685ed65a27c932602
Federico Barresi <federico.barresi@ams-osram.com>
2025-11-08 22:46:25 +0100

    removed unused upload get

--- upload.py
+++ upload.py
@@ -3,10 +3,6 @@ from microdot import Microdot, send_file, Request
 upload_app = Microdot()
 Request.max_content_length = 512 * 1024 # 500kB
 
-@upload_app.get('/')
-async def get_connections(request):
-    return send_file('static/upload.html')
-
 @upload_app.post('/image')
 async def upload(request):
     # obtain the filename and size from request headers
@@ -16,13 +12,15 @@ async def upload(request):
     print("received:",filename, size)
     # sanitize the filename
     filename = filename.replace('/', '_')
+    
+    imagename = 'image.bin'
 
     # write the file to the files directory in 1K chunks
-    with open('images/' + filename, 'wb') as f:
+    with open('images/' + imagename, 'wb') as f:
         while size > 0:
             chunk = await request.stream.read(min(size, 1024))
             f.write(chunk)
             size -= len(chunk)
 
     print('Successfully saved file: ' + filename)
-    return 'We have received your data'
+    return 'Image successfully saved'
